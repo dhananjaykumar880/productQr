090e121155f6a1fc6fd601fe0c9a1b558587de70
Dhananjay Maurya <dhananjay.maurya@dynnino.in>
2023-02-16 03:49:15 +0530

    added view page

--- client/src/components/product/ViewProduct.tsx
+++ client/src/components/product/ViewProduct.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { BE_BASE_URL } from '../../constant'
+import { Product } from '../../interfaces/product'
+import { useGetProductByIdQuery } from '../../redux/product/productApi'
 
 export default function ViewProduct() {
+    const params = useParams()
+    const productId = Number(params.productId)
+    const { data, isSuccess } = useGetProductByIdQuery(productId, { skip: !Boolean(productId)})
     return <>
-        Page not found
+        <h3>Product details</h3>
+        <table>
+            <tbody>
+                {isSuccess && Object.keys(data).filter(key => !['createdAt', 'updatedAt'].includes(key)).map((key, index) => <tr key={index}>
+                    <th>{key.toUpperCase()}</th>
+                    <td>
+                        {key === 'image' 
+                            ? data[key as keyof Product] 
+                                ? <img src={`${BE_BASE_URL}${data[key as keyof Product]}`} 
+                                        alt={data.product} 
+                                        width={100}
+                                    /> 
+                                : null 
+                            : data[key as keyof Product]}
+                    </td>
+                </tr>)}
+            </tbody>
+        </table>
     </>
 }
